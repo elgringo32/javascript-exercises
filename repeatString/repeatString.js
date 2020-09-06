20b2cb8076fe7cbe5735c0656f693de517074b58
elgringo32 <bknopke@gmail.com>
2020-09-06 08:44:27 -0700

    fixed repeat string

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
 
+const repeatString = function(string, loops) {
+        let result = ''
+        if (loops < 0) {
+            return 'ERROR';
+        }
+        else {
+            for (i=0; i < loops; i++) {
+                result = result.concat(string)
+            }
+        return result;
+        }
+        console.log(result);
 }
+
 
 module.exports = repeatString
