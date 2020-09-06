ff0e86ffcd4aec3da420fadad52894b1d3814f57
elgringo32 <bknopke@gmail.com>
2020-09-06 08:49:22 -0700

    fix files"

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,4 +1,9 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    strResult = ''
+    for (i=str.length-1; i >=0; i--) {
+        strResult += str[i]
+    }
+    return strResult
 
 }
 
