9f43a9098ff6b933a447d96228979557417133ee
elgringo32 <bknopke@gmail.com>
2020-09-06 15:25:41 -0700

    fixed palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,4 +1,14 @@
-const palindromes = function() {
+const palindromes = function(str) {
+    str = str.replace(/[' ',.!?\\-]/g,'').toLowerCase();
+    console.log(str);
+    let pStr = str.split('').reverse().join('').replace(/[.!?\\-]/g,'');
+    console.log(pStr);
+    if (str === pStr) {
+        return true;
+    } 
+    else {
+        return false;
+    }
 
 }
 
