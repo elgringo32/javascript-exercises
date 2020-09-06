86825c525ea2fe0cd6b236e4874f85953f0a30ea
elgringo32 <bknopke@gmail.com>
2020-09-06 08:48:00 -0700

    remove array

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, int) {
+    for (i=0; i < arr.length; i++) {
+        if (arr[i] = int) {
+            arr = arr.splice(i,1);
+            return arr;
+        }
+    }
+    
+    console.log(arr);
 }
 
 module.exports = removeFromArray
