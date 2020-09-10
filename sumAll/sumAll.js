ce7dab260a66ec8fcdb9df41c07ab9c11c55930b
elgringo32 <bknopke@gmail.com>
2020-09-09 21:53:44 -0700

    sumall

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(firstInt, secondInt) {
+    let start = Math.min(firstInt, secondInt);
+    let end = Math.max(firstInt, secondInt);
+    let result = 0
+    if (!Number.isInteger(firstInt) || !Number.isInteger(secondInt) || firstInt< 0 || secondInt <0) {
+        return 'ERROR'
+    }
+    else {
+        for (i=start; i <=end; i++) {
+            result += i;
+        }
+        return result
+    }
+    
 }
 
 module.exports = sumAll
