e11e2e1231a76b847004bb35702253dad66683e7
elgringo32 <bknopke@gmail.com>
2020-09-13 12:41:23 -0700

    leap year tests

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let divByFour = year % 4;
+    let divbyOneHundred = year % 100;
+    let divByFourHundred = year % 400;
+    if (divByFour === 0 && divbyOneHundred != 0 && divByFourHundred != 0) {
+        return true;
+    } 
+    else if (divByFour === 0 && divbyOneHundred === 0 && divByFourHundred === 0) {
+        return true;
+    }
+    else if ( divByFour === 0 && divbyOneHundred === 0 && divByFourHundred != 0) {
+        return false;
+    }
+    else {
+        return false;
+    }
+   
 }
-
 module.exports = leapYears
